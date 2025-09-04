51199bbea0c5c13733b9b656b9853a3f2f120300
ani8727 <aniketgupta.8727@gmail.com>
2025-09-04 21:22:48 +0530

    min of rotated sorted array

--- Array/PivotElement.cpp
+++ Array/PivotElement.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 // find pivot element - smallest number in sorted rotated array
-
+//  153. Find Minimum in Rotated Sorted Array
 int pivot(vector<int> nums){
     int n=nums.size();
     int st=0;
     int end=n-1;
     while(st<end){
         int mid=st+(end-st)/2;
-        if(nums[mid]>=nums[0]){ // means ans in sorted right side array
+        if(nums[mid]>nums[end]){ // means ans in sorted right side array
             st=mid+1;
         }else{
             end=mid;  // means inleft side present
         }
     }
-    return end;     // index of lowest element
+    return st;     // index of lowest element
 }
 int main(){
     vector<int> nums={5,6,0,1,2,3,4};
